6deea89d3916f2e44e814b850a948d1bf3c63934
mauricio pineda <map1900@outlook.com>
2021-08-08 16:56:07 -0400

    Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Amoblado antiguo y clasico',
+  address: 'Recoleta',
+  description: 'Amoblado antiguo y clasico',
+  price_per_night: 65000,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Amoblado rustico y sereno',
+  address: 'Providencia',
+  description: 'Al paso de veredas arboladas y serenas',
+  price_per_night: 96000,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Espacioso y centrico',
+  address: 'Vitacura',
+  description: 'En el corazon del sector financiero',
+  price_per_night: 75000,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Para jovenes profesionales',
+  address: 'Las Condes',
+  description: 'Cerca de sector de oficinas, hermoso',
+  price_per_night: 89000,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Acogedor y lindo',
+  address: 'Ñuñoa',
+  description: 'Sector calmado y tranquilo',
+  price_per_night: 74000,
+  number_of_guests: 3
+)
+
