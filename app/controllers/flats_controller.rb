5bf32b1661034def47deef57b1ee78241f1ee604
mauricio pineda <map1900@outlook.com>
2021-08-09 20:01:19 -0400

    resolved an issue with @popperjs/core

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,40 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, except: [ :index, :new, :create ]
+  # only: [ :show, :update, :edit, :destroy ]
   def index
     @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :rating)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
